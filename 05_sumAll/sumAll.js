927288907807a12e98a9b19e4e638ecbd371e43b
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-10 23:40:33 +0500

    SUmming everything in range done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(minRange, maxRange) {
+    if (!Number.isInteger(minRange) || !Number.isInteger(maxRange)) return "ERROR"
+    if (maxRange < 0 || minRange < 0) return "ERROR"
 
+    if (minRange > maxRange){
+        let temp = maxRange;
+        maxRange = minRange;
+        minRange = temp;
+    }
+
+    let sum = 0;
+    for (let i = minRange; i <= maxRange; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line

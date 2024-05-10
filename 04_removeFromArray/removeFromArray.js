e49da6dc0ee94513a684b05964202cd376a662a6
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-10 23:25:43 +0500

    Deleting an element Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toBeRemoved) {
+    const newArray = [];
+    array.forEach(element => {
+        if (!toBeRemoved.includes(element))
+            newArray.push(element);
+    });
+    return newArray;
 };
 
 // Do not edit below this line
